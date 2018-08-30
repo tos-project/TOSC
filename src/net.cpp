1ba10161a67f0d3793ff49d8ec2f4962bb64f3f4
Bryan G. Lee <bryanlee@tosblock.com>
2018-08-30 17:31:55 +0900

    Add support for latest version miniupnpc and add official DNSSeed

--- src/net.cpp
+++ src/net.cpp
@@ -1084,10 +1084,14 @@ void ThreadMapPort()
 #ifndef UPNPDISCOVER_SUCCESS
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-#else
+#elif MINIUPNPC_API_VERSION < 14
     /* miniupnpc 1.6 */
     int error = 0;
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
+#else
+    /* miniupnpc > 1.6 20180805 */
+    int error = 0;
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
 #endif
 
     struct UPNPUrls urls;
@@ -1114,7 +1118,7 @@ void ThreadMapPort()
             }
         }
 
-        string strDesc = "TosCoin " + FormatFullVersion();
+        string strDesc = "TOS " + FormatFullVersion();
 
         try {
             loop {
@@ -1194,14 +1198,16 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"cryptolife.net","electrum6.cryptolife.net"},
-    {"cryptolife.net","explore.cryptolife.net"},
-    {"cryptolife.net","seed1.cryptolife.net"},
-    {"cryptolife.net","seed2.cryptolife.net"},
+    {"tosblock.com","dnsseed.tosblock.com"},
+    {"tosblock.com","dnsseed2.tosblock.com"},
+    {"tosblock.com","dnsseed3.tosblock.com"},
+    {"tosblock.com","dnsseed4.tosblock.com"},
+    {"tosblock.com","dnsseed5.tosblock.com"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
+	{"tosblock.com","testnet-dnsseed.tosblock.com"},
     {NULL, NULL}
 };
 
@@ -1261,7 +1267,7 @@ void DumpAddresses()
     CAddrDB adb;
     adb.Write(addrman);
 
-    printf("Flushed %d addresses to peers.dat  %"PRI64d"ms\n",
+    printf("Flushed %d addresses to peers.dat  %" PRI64d "ms\n",
            addrman.size(), GetTimeMillis() - nStart);
 }
 
@@ -1684,7 +1690,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. TosCoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. T.OS Core is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
